3f2a07d07f10e598100250c8433f12c77b7c2c4d
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-13 00:00:04 +0100

    feat(auth_repository): Add auth repository
    - Implemented auth_repository with login and register async methods
    - Registered the repository in auth_providers

---
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:mixxfit_mobile/features/auth/data/models/auth_response.dart';
+import 'package:mixxfit_mobile/features/auth/data/models/login_request.dart';
+import 'package:mixxfit_mobile/features/auth/data/models/register_request.dart';
+
+class AuthRepository {
+  final Dio _dio;
+
+  AuthRepository(this._dio);
+
+  Future<AuthResponse> register(RegisterRequest request) async {
+    final response = await _dio.post("/auth/register", data: request.toJson());
+    return AuthResponse.fromJson(response.data);
+  }
+
+  Future<AuthResponse> login(LoginRequest request) async {
+    final response = await _dio.post("/auth/login", data: request.toJson());
+    return AuthResponse.fromJson(response.data);
+  }
+
+}
