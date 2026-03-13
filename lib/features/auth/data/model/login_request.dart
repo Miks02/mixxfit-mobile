3d502eee113613aa655fdcc9cc1e39990a0f3c5c
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-13 13:01:46 +0100

    feat(auth): Add LoginRequest model with JSON serialization

---
+++ lib/features/auth/data/model/login_request.dart
@@ -0,0 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'login_request.g.dart';
+
+@JsonSerializable(createToJson: true, createFactory: false)
+class LoginRequest {
+  final String email;
+  final String password;
+
+  const LoginRequest({required this.email, required this.password});
+
+  Map<String, dynamic> toJson() => _$LoginRequestToJson(this);
+}
