d85e854a8e54c2f85a2dfc9e32f07274af5eec65
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-03-14 13:51:17 +0100

    feat(router): Implement GoRouter with authentication redirection logic

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,5 +1,37 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mixxfit_mobile/features/auth/presentation/pages/auth_page.dart';
+import 'package:mixxfit_mobile/features/auth/presentation/state/auth_notifier.dart';
+
+final routerProvider = Provider<GoRouter>((ref) {
+  final authState = ref.watch(authProvider);
+
+  return GoRouter(
+    initialLocation: authState.asData!.value.isAuthenticated
+        ? "/dashboard"
+        : "/auth",
+    redirect: (context, state) {
+      final isLoggedIn = authState.asData!.value.isAuthenticated ? true : false;
+
+      if (state.matchedLocation == "/auth") {
+        return isLoggedIn ? "/dashboard" : "/auth";
+      }
+
+      if (state.matchedLocation != "/auth" && !isLoggedIn) {
+        return "/auth";
+      }
+
+      return null;
+    },
+    routes: [
+      GoRoute(
+        path: "/auth",
+        name: "/auth",
+        builder: (context, state) => AuthPage(),
+      ),
+    ],
+  );
+});
 
 final appRouter = GoRouter(
   initialLocation: "/auth",
