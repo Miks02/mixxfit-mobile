871abd1245a54df463bfdf4ce42f633078633488
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-16 21:43:26 +0200

    feat(error-interceptor): Add problem-details-interceptor to map incoming errors into the problem details type if it fits the criteria

--- src/core/interceptors/setup.ts
+++ src/core/interceptors/setup.ts
@@ -1,8 +1,10 @@
 import { tokenInterceptor } from "./auth-interceptor"
+import { problemDetailsInterceptor } from "./problem-details-interceptor";
 import { refreshInterceptor } from "./refresh-interceptor";
 
 
 export const setupInterceptors = () => {
     tokenInterceptor();
     refreshInterceptor();
+    problemDetailsInterceptor();
 }
