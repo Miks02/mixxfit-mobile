f7729d42685c479c58771897d978c7c1e9b0c258
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-16 08:53:23 +0200

    feat(auth-store): Implement async user data hydration and secure storage for credentials

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,19 @@
 import "@/global.css";
+import { useAuthStore } from "@/src/features/auth/store/auth-store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
+    const hydrate = useAuthStore((state) => state.hydrateUserData)
 
+    useEffect(() => {
+        hydrate();
+    }, [])
 
     return (
         <QueryClientProvider client={queryClient}>
--- src/features/auth/store/auth-store.ts
+++ src/features/auth/store/auth-store.ts
@@ -1,20 +1,42 @@
 import { UserDetailsDto } from '@/src/core/types/user-details-dto';
 import { create } from 'zustand';
+import * as SecureStore from "expo-secure-store";
 
 type AuthState = {
     user: UserDetailsDto | null,
     token: string | null,
     isAuthenticated: boolean
 
-    setCredentials: (user: UserDetailsDto, token: string) => void,
-    clearCredentials: () => void
+    setCredentials:  (user: UserDetailsDto, token: string) => Promise<void>,
+    clearCredentials: () => Promise<void>,
+    hydrateUserData: () => Promise<void>
 }
 
+const USER_KEY = "user";
+const TOKEN_KEY = "token";
+
 export const useAuthStore = create<AuthState>((set) => ({
-    user: null,
+    user:  null,
     token: null,
     isAuthenticated: false,
 
-    setCredentials: (user, token) => set({user, token, isAuthenticated: true}),
-    clearCredentials: () => set({user: null, token: null, isAuthenticated: false})
+    setCredentials: async (user, token) => {
+        await SecureStore.setItemAsync(USER_KEY, JSON.stringify(user))
+        await SecureStore.setItemAsync(TOKEN_KEY, JSON.stringify(token))
+        set({user, token, isAuthenticated: true})
+    },
+    clearCredentials: async () => {
+        await SecureStore.deleteItemAsync(USER_KEY);
+        await SecureStore.deleteItemAsync(TOKEN_KEY);
+        set({user: null, token: null, isAuthenticated: false})
+    },
+    hydrateUserData: async () => {
+        const rawUser = await SecureStore.getItemAsync(USER_KEY)
+        const rawToken = await SecureStore.getItemAsync(TOKEN_KEY)
+
+        if(rawUser && rawToken)
+            set({user: JSON.parse(rawUser), token: JSON.parse(rawToken), isAuthenticated: true})
+
+    }
 }))
+
