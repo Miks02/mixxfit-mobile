ca3f76b24c14659b8ba591c2157c755b67a16a31
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-17 10:12:08 +0200

    wip(pages): Add multiple blank pages that will be used across the application that are yet to be filled
    - Pages are added now just so i can have an easier time implementing the tab bar navigation

---
+++ app/(tabs)/workouts/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+
+const WorkoutsLayout = () => {
+  return (
+    <Stack>
+        <Stack.Screen name='index'></Stack.Screen>
+        <Stack.Screen name='workout-form'></Stack.Screen>
+    </Stack>
+  )
+}
+
+export default WorkoutsLayout
