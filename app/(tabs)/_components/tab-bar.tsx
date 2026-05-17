a744d2d25b5a65982920beb6cfaeba95d6c8c2db
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-17 12:00:07 +0200

    feat(tab-bar): Enhance tab bar with dynamic links and icon integration

--- app/(tabs)/_components/tab-bar.tsx
+++ app/(tabs)/_components/tab-bar.tsx
@@ -1,30 +1,85 @@
 import { Colors } from '@/src/constants/colors'
+import { FontAwesome5 } from '@expo/vector-icons'
+import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, View } from 'react-native'
 
-const TabBar = () => {
+const links = [
+    {
+        name: "Dashboard",
+        icon: "chart-bar",
+        route: 'dashboard'
+    },
+    {
+        name: "Workouts",
+        icon: "dumbbell",
+        route: 'workouts'
+    },
+    {
+        name: "Weight Tracking",
+        icon: "weight",
+        route: 'weight-tracking'
+    },
+    {
+        name: "Exercise Library",
+        icon: "book-open",
+        route: 'exercise-library'
+    },
+    {
+        name: "Profile",
+        icon: "user",
+        route: 'profile'
+    },
+    {
+        name: "Workout Form",
+        icon: "plus",
+        route: 'workout-form'
+    },
+]
+
+const activeLink = (linkIndex: number, stateIndex: number) => linkIndex === stateIndex ? Colors.emerald[700]  : Colors.slate[800]
+
+const TabBar = ({navigation, state, descriptors}: BottomTabBarProps) => {
     return (
         <View className='flex-row gap-2 w-full p-6 absolute bottom-0'>
-        <LinearGradient
+        <View className='flex-row grow gap-2' style={{borderRadius: 24}}>
+            <LinearGradient
         colors={[Colors.yellow[500], Colors.yellow[600]]}
         start={{x: 0, y:0}}
         end={{x:0, y:1}}
-        className='flex-row grow justify-between p-5 shadow-xl'
+        className='flex-row grow justify-between p-5 shadow-2xl'
         style={{borderRadius: 24}}>
-        <Text>tab</Text>
-        <Text>tab</Text>
-        <Text>tab</Text>
-        <Text>tab</Text>
-        <Text>tab</Text>
-        <Text>tab</Text>
+
+        {links.map((l, index) => (
+
+            l.name !== "Workout Form" &&
+            <Pressable
+            key={l.name}
+            onPress={() => {
+                const event = navigation.emit({
+                    type: 'tabPress',
+                    target: l.route,
+                    canPreventDefault: true
+                })
+                navigation.navigate(l.route, {screen: 'index'})
+            }}
+            className={`active:opacity-50 transition duration-200`}>
+                <FontAwesome5 color={activeLink(index, state.index)} size={24} name={l.icon}></FontAwesome5>
+            </Pressable>
+        ))}
+
         </LinearGradient>
-        <LinearGradient
-    colors={[Colors.yellow[500], Colors.yellow[600]]}
-    className='flex-row justify-between p-5 shadow-xl'
-    style={{borderRadius: 24}}>
-      <Text>tab</Text>
-    </LinearGradient>
+        <Pressable onPress={() => navigation.navigate('workouts', {screen: 'workout-form'})} className='active:opacity-50 transition duration-200 shadow-2xl'>
+            <LinearGradient
+        colors={[Colors.yellow[400], Colors.yellow[500]]}
+        className='flex-row justify-between p-5 shadow-xl '
+        style={{borderRadius: 100}}>
+                <FontAwesome5 color={Colors.slate[800]} size={24} name={"plus"}></FontAwesome5>
+        </LinearGradient>
+        </Pressable>
+
+        </View>
         </View>
     )
 }
