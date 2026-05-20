34d4e5dbc10e621397a0ac92cbabc97185d1a281
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-20 23:24:25 +0200

    feat(dashboard): Refactor Dashboard component to use DashboardScreen and add DashboardCard component

---
+++ src/features/dashboard/components/dashboard-card.tsx
@@ -0,0 +1,24 @@
+import { FontAwesome5 } from '@expo/vector-icons'
+import React from 'react'
+import { Text, View } from 'react-native'
+
+type DashboardCardProps = {
+    title: string,
+    value: string,
+    color: string,
+    icon: string
+}
+
+const DashboardCard = ({title, value, color, icon}: DashboardCardProps) => {
+    return (
+        <View style={{backgroundColor: color}} className='grow rounded-xl gap-6 shadow-xl p-4'>
+        <View className='flex-row gap-4 items-center'>
+        <FontAwesome5 name={icon} size={24} color={'white'}></FontAwesome5>
+        <Text className='text-xl text-gray-100 font-semibold grow'>{title}</Text>
+        </View>
+        <Text className='text-xl text-gray-100 font-semibold'>{value}</Text>
+        </View>
+    )
+}
+
+export default DashboardCard
