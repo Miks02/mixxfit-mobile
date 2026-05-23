4ca10a6216bb72132f16aea1725ade27b4e2b87b
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-23 15:31:50 +0200

    feat(workouts): Add WorkoutListItem type definition

---
+++ src/features/workouts/types/workout-list-item.ts
@@ -0,0 +1,10 @@
+export type WorkoutListItem = {
+    id: number,
+    name: string,
+    workoutDate: string,
+    exerciseCount: number,
+    setCount: number,
+    hasCardio: boolean,
+    hasWeights: boolean,
+    hasBodyWeight: boolean
+}
