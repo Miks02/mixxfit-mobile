62e1406ad08af0be28b39decce71d1c51da3df7b
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-05-29 05:15:09 +0200

    refactor(workout-store): Improve formatting and consistency in WorkoutParams type definition

--- src/features/workouts/store/workout-store.ts
+++ src/features/workouts/store/workout-store.ts
@@ -1,27 +1,28 @@
 import { create } from "zustand";
 
 type WorkoutParams = {
-    year?: number;
-    month?: number;
-    search?: string;
-    sort?: string;
-    actions: WorkoutParamsActions;
-}
+  year?: number;
+  month?: number;
+  search?: string;
+  sort?: string;
+  actions: WorkoutParamsActions;
+};
 
 type WorkoutParamsActions = {
-    setYear: (year: number) => void;
-    setMonth: (month: number) => void;
-    initWorkoutParams: (year:number, month:number) => void;
-}
+  setYear: (year: number) => void;
+  setMonth: (month: number) => void;
+  initWorkoutParams: (year: number, month: number, sort?: string) => void;
+};
 
 export const useWorkoutParamsStore = create<WorkoutParams>((set) => ({
-    year: undefined,
-    month: undefined,
-    search: undefined,
-    sort: undefined,
-    actions: {
-        setYear: (year: number) => set({ year: year }),
-        setMonth: (month: number) => set({ month: month }),
-        initWorkoutParams: (year:number, month:number) => set({year: year, month: month})
-    }
-}))
+  year: undefined,
+  month: undefined,
+  search: undefined,
+  sort: undefined,
+  actions: {
+    setYear: (year: number) => set({ year: year }),
+    setMonth: (month: number) => set({ month: month }),
+    initWorkoutParams: (year: number, month: number, sort: string = "newest") =>
+      set({ year: year, month: month, sort: sort }),
+  },
+}));
