cba7553d24802fbcb3c86c5763e719cb545a0392
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-06-02 12:14:27 +0200

    feat(workout-types): Add additional properties in workout-summary type

--- src/features/workouts/types/workout-summary.ts
+++ src/features/workouts/types/workout-summary.ts
@@ -1,8 +1,11 @@
 import { ExerciseType } from "./exercise-type"
+import { MostActiveMonth } from "./most-active-month"
 
 export type WorkoutSummary = {
     workoutCount: number,
     exerciseCount: number,
     lastWorkoutDate: string,
-    favoriteExerciseType: ExerciseType
+    favoriteExerciseType: ExerciseType,
+    workoutStreak: number,
+    mostActiveMonths: MostActiveMonth[]
 }
