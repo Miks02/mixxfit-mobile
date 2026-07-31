9b9fcfc727ddf9081666d8d73a05b93e0ea24db1
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-31 08:57:43 +0200

    feat(exercise): Add string values for exercise type enum

--- src/features/workouts/types/exercise-type.ts
+++ src/features/workouts/types/exercise-type.ts
@@ -1,7 +1,7 @@
 export enum ExerciseType {
-    Other = 0,
-    Weights = 1,
-    Bodyweight = 2,
-    Cardio = 3,
-    Stretching = 4
+  Other = "Other",
+  Weights = "WeightLifting",
+  Bodyweight = "Bodyweight",
+  Cardio = "Cardio",
+  Stretching = "Stretching",
 }
