f413ec9f7ef305f0d1c7c262c3272206beb76b79
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-07-31 13:16:42 +0200

    feat(auth, store): Add reset function in workoutParamsStore and call it on logout in useAuth

--- src/features/auth/hooks/use-auth.ts
+++ src/features/auth/hooks/use-auth.ts
@@ -2,6 +2,7 @@ import { api } from "@/src/constants/api";
 import { ProblemDetails } from "@/src/core/types/problem-details";
 import { UserDetailsDto } from "@/src/core/types/user-details-dto";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useWorkoutParamsStore } from "../../workouts/store/workout-params-store";
 import { LoginFormData } from "../schemas/login-schema";
 import { RegisterFormData } from "../schemas/register-schema";
 import { useAuthStore } from "../store/auth-store";
@@ -10,7 +11,6 @@ export default function useAuth() {
     const queryClient = useQueryClient();
     const setCredentials = useAuthStore((state) => state.setCredentials);
     const clearCredentials = useAuthStore((state) => state.clearCredentials);
-    const token = useAuthStore((state) => state.token);
 
     const login = useMutation<{user: UserDetailsDto, accessToken: string}, ProblemDetails, LoginFormData>({
         mutationFn: async (request: LoginFormData) => {
@@ -34,11 +34,13 @@ export default function useAuth() {
 
     const logout = useMutation({
         mutationFn: async () => {
+            const token = useAuthStore.getState().token;
             await clearCredentials();
             await api.post('/auth/logout', {}, {headers: {'Authorization': `Bearer ${token}`}})
         },
-        onMutate: async () => {
+        onSettled: async () => {
             queryClient.clear();
+            useWorkoutParamsStore.getState().actions.reset();
         }
     })
 
