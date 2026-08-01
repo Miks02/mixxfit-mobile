25158413585d842bf3f271ee55937e0c744c214c
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-08-01 09:58:16 +0200

    feat(hook): Implement a simple use-workout-details

---
+++ src/features/workouts/hooks/use-workout-details.ts
@@ -0,0 +1,33 @@
+import { api } from "@/src/constants/api";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "date-fns";
+import { WorkoutDetails } from "../types/workout-details";
+
+const getWorkoutDetails = async (id: number): Promise<WorkoutDetails> => {
+    const res = await api.get(`workouts/${id}`);
+    console.log(res.data);
+    return res.data;
+}
+
+export default function useWorkoutDetails(id: number) {
+
+    const detailsQuery = useQuery({
+        queryKey: ["workout-details", id],
+        queryFn: async () => await getWorkoutDetails(id),
+        select: (data: WorkoutDetails) => ({
+            ...data,
+            workoutDate: format(data.workoutDate, "dd.MM.yyyy")
+      }),
+        staleTime: 1000 * 60 * 5
+    });
+
+    const refetchDetails = async () => await detailsQuery.refetch();
+
+    return {
+        details: detailsQuery.data,
+        refetch: refetchDetails,
+        isLoading: detailsQuery.isLoading,
+        isRefetching: detailsQuery.isRefetching,
+        isError: detailsQuery.isError
+    }
+}
